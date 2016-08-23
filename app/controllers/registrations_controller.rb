129e74269400fb2ed12593bb444a43455b9e4438
Alex Bacon <bakna1@hotmail.com>
2016-08-23 08:16:26 +0000

    initial userupdate

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,8 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:email,
+    params.require(:user).permit(
+    :email,
     :password,
     :password_confirmation,
     :username,
